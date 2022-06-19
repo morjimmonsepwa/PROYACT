83f7b60d9486e2f74376898080683b9220499ef8
Monserrat <morjimmonse81@gmail.com>
2022-06-19 14:38:09 -0600

    Se agregaron archivos de programas

---
+++ Index.html
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PROYACT</title>
+  </head>
+  <body>
+  <p>Práctica Git</p>
+  </body>
+</html>
---
+++ Test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PROYACT</title>
+  </head>
+  <body>
+  <p>Se debe realizar un repositorio en git, con el nombre PROYACT </p>
+  </body>
+</html>
